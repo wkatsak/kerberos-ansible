04b014a9c046f3c680e8dc3efa922f8e9bc71f1a
root <root@config.lcsr.rutgers.edu>
2018-08-23 14:23:40 -0400

    checkin

--- kerberos-files/usr/libexec/ilab-setlimits.sh
+++ kerberos-files/usr/libexec/ilab-setlimits.sh
@@ -12,7 +12,11 @@ LOGIN=`getent passwd "$PAM_USER" | cut -d: -f3`
 if ! getent -s files passwd "$PAM_USER"; then
   if ! (groups "$PAM_USER" | grep '\bno-mem-limit\b' >/dev/null) ; then
    # limit is 1/2 of phys mem. use right shift so it stays integer
+{% if memorylimit is defined %}
+   LIMIT={{ memorylimit }}
+{% else %}
    LIMIT=`vmstat -s | grep "total memory" | awk '{print rshift($1,1) "K"}'`
+{% endif %}
    # need to do this to create the slice and turn on memory accounting
    systemctl set-property --runtime "user-${LOGIN}.slice" MemoryLimit=$LIMIT
    echo $LIMIT > /sys/fs/cgroup/memory/user.slice/user-${LOGIN}.slice/memory.memsw.limit_in_bytes   
@@ -20,6 +24,26 @@ if ! getent -s files passwd "$PAM_USER"; then
    cat /sys/fs/cgroup/memory/user.slice/user-${LOGIN}.slice/memory.limit_in_bytes > /dev/null
    cat /sys/fs/cgroup/memory/user.slice/user-${LOGIN}.slice/memory.memsw.limit_in_bytes > /dev/null
    systemctl set-property --runtime "user-${LOGIN}.slice" CPUShares=100
+{% if nvidialimit is defined %}
+
+   # device limits seem to apply to the session, not the user, though if you
+   # set it on the user future sessions will inherit it. But that won't work
+   # for the first session. So use session.
+   if test -n "$XDG_SESSION_ID"; then
+      # 8 devices. use the same for a given user, UID mod 8
+      DEVNO=`expr $LOGIN % 8`
+      NUMDEV={{ nvidialimit }}
+      systemctl set-property --runtime "session-${XDG_SESSION_ID}.scope" DevicePolicy=closed
+      systemctl set-property --runtime "session-${XDG_SESSION_ID}.scope" DeviceAllow="/dev/nvidiactl rw"
+      systemctl set-property --runtime "session-${XDG_SESSION_ID}.scope" DeviceAllow="/dev/nvidia-uvm rw"
+      systemctl set-property --runtime "session-${XDG_SESSION_ID}.scope" DeviceAllow="/dev/nvidia-uvm-tools rw"
+      for ((DEV=$DEVNO; DEV < (DEVNO + NUMDEV); DEV++)); do
+        IND=`expr $DEV % 8`
+        systemctl set-property --runtime "session-${XDG_SESSION_ID}.scope" DeviceAllow="/dev/nvidia$IND rw"
+      done
+   fi
+
+{% endif %}
   fi
 fi
 
